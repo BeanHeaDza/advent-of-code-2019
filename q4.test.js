05fb881ee89b6b5ddce94c0769ce8d841ba2e2f7
Adrian <reeper24@gmail.com>
2019-12-05 07:09:29 +0200

    Day 4 Part 2

--- q4.test.js
+++ q4.test.js
@@ -1,4 +1,4 @@
-const { isValid } = require("./q4");
+const { isValid, isValidPart2 } = require("./q4");
 
 describe("isValid", () => {
   it("should be valid for 111111", () => {
@@ -15,5 +15,17 @@ describe("isValid", () => {
 
   it("should be valid for 177778", () => {
     expect(isValid(177778)).toBe(true);
+  });
+});
+
+describe("isValidPart2", () => {
+  it("should be valid for 112233", () => {
+    expect(isValidPart2(112233)).toBe(true);
+  });
+  it("should be invalid for 123444", () => {
+    expect(isValidPart2(123444)).toBe(false);
+  });
+  it("should be valid for 111122", () => {
+    expect(isValidPart2(111122)).toBe(true);
   });
 });
